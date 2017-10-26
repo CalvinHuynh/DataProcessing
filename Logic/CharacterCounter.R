5eafbf89b8908da23bb2d8ac5aeca8bce699fa6d
Calvin <huynhck001@gmail.com>
2017-10-26 16:55:59 +0200

    merged two dataframes

--- Logic/CharacterCounter.R
+++ Logic/CharacterCounter.R
@@ -1,6 +1,7 @@
 library(RMongo)
 library(dplyr)
 library(stringr)
+library(tidyr)
 #library(stringi)
 
 # Genersl functions and variables -----------------------------------------
@@ -10,11 +11,17 @@ path <- function(input) {
   return(paste(filePath, input, sep = ""))
 }
 
+# trims the whitespaces
 trim <- function(string) {
   gsub("^\\s+|\\s+$", "", string)
 }
 
-vowels <- c("a","e","i","o","u")
+# splits the year
+extractYear <- function(string) {
+  substr(string, nchar(string) - 6 + 1, nchar(string))
+}
+
+vowels <- c("a", "e", "i", "o", "u")
 
 vowels_count <- function(string) {
   sum(str_count(tolower(string), vowels))
@@ -26,9 +33,14 @@ letters_count <- function(string) {
 
 # Retrieve data from mongoDb ----------------------------------------------
 
-mcon <- mongoDbConnect("Practicum",port=27017)
+mcon <- mongoDbConnect("Practicum", port = 27017)
 moviesCollection <- "DataKaggle"
-kaggleData<- dbGetQuery(mcon, moviesCollection,"{}",skip=0,limit=100000)
+kaggleData <-
+  dbGetQuery(mcon,
+             moviesCollection,
+             "{}",
+             skip = 0,
+             limit = 100000)
 
 # Reading local data from movielens ---------------------------------------
 
@@ -50,20 +62,75 @@ summedUpRating <- moviesRatings %>%
 mergedMovieRatings <- merge(numberOfPeopleVoted, summedUpRating)
 mergedMovieRatings <- mergedMovieRatings %>%
   mutate(avg_rating = sapply(total_rating / no_of_votes, function(x) {
-    round(x, digits = 2)
+    round(x, digits = 1)
   }))
 
-mergedMovieRatings <- mergedMovieRatings[!(mergedMovieRatings$avg_rating == 0),]
+mergedMovieRatings <-
+  mergedMovieRatings[!(mergedMovieRatings$avg_rating == 0), ]
 
 mergedMovieRatings <- mergedMovieRatings %>%
   mutate(no_of_vowels = sapply(title, vowels_count))
 
 mergedMovieRatings <- mergedMovieRatings %>%
   mutate(no_of_letters = sapply(title, letters_count)) %>%
-  mutate(no_of_consonants = no_of_letters - no_of_vowels)
+  mutate(no_of_consonants = no_of_letters - no_of_vowels) %>%
+  mutate(title_year = sapply(as.character(title), extractYear))
 
-cleanedMovieLensDf <- subset(mergedMovieRatings,
-                       select = 
-                         c(title, no_of_votes,
-                           avg_rating, no_of_vowels,
-                           no_of_letters, no_of_consonants))
+# Retrieves all the abnormal rows
+rowsToFix <- subset(mergedMovieRatings,!grepl("^\\(", title_year))
+
+# Filter rows without any digits
+rowsToFix <- subset(rowsToFix, grepl("^\\d", title_year))
+
+# Specific fixes
+rowsToFix$title_year <- sub("^", "\\(", rowsToFix$title_year)
+rowsToFix$title_year <-
+  gsub(rowsToFix$title_year, pattern = "-", replacement = "")
+
+# Remove abnormal rows from df
+mergedMovieRatings <-
+  subset(mergedMovieRatings, grepl("^\\(", title_year))
+
+# Merge fixedRows with df with a full join
+mergedMovieRatings <- merge(mergedMovieRatings, rowsToFix, key = "title", all = TRUE)
+mergedMovieRatings$title_year <- gsub(mergedMovieRatings$title_year, pattern = "\\(", replacement = "")
+mergedMovieRatings$title_year <- gsub(mergedMovieRatings$title_year, pattern = "\\)", replacement = "")
+
+# Final dataframes --------------------------------------------------------
+
+cleanedMovieLensDf <- subset(
+  mergedMovieRatings,
+  select =
+    c(
+      title,
+      title_year,
+      no_of_votes,
+      avg_rating,
+      no_of_vowels,
+      no_of_letters,
+      no_of_consonants
+    )
+)
+
+filteredKaggleDf<- subset(
+  kaggleData,
+  select =
+    c(
+      title,
+      title_year,
+      imdb_score,
+      content_rating,
+      budget,
+      gross
+    )
+)
+
+# Combining movielens data with kaggle data -------------------------------
+
+totalDf <- merge(filteredKaggleDf, cleanedMovieLensDf, key = "title", all.x = TRUE)
+
+dfToComplement <- totalDf %>%
+  filter(is.na(no_of_vowels)) %>%
+  mutate(no_of_vowels = sapply(title, vowels_count)) %>%
+  mutate(no_of_letters = sapply(title, letters_count)) %>%
+  mutate(no_of_consonants = no_of_letters - no_of_vowels)
